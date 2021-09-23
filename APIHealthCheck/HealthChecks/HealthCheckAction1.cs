bd01a33555c609cab866a8a5e1f2d5d5912b09ef
Suman Chatterjee <suman.chatterjee341@hotmail.com>
2021-09-23 13:17:30 +0530

    Add project files.

---
+++ APIHealthCheck/HealthChecks/HealthCheckAction1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace APIHealthCheck.HealthChecks
+{
+    public class HealthCheckAction1 : IHealthCheck
+    {
+        private Random _rnd = new Random();
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            int responseTimeinMS = _rnd.Next(1,300);
+            if (responseTimeinMS < 100)
+                return Task.FromResult(HealthCheckResult.Healthy($"Response Time: {responseTimeinMS}"));
+            else if (responseTimeinMS < 200)
+                return Task.FromResult(HealthCheckResult.Degraded($"Response Time: {responseTimeinMS}"));
+            else
+                return Task.FromResult(HealthCheckResult.Unhealthy($"Response Time: {responseTimeinMS}"));
+        }
+    }
+}
---
+++ APIHealthCheck/Startup.cs
@@ -0,0 +1,62 @@
+using APIHealthCheck.HealthChecks;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIHealthCheck
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddHealthChecks().
+                AddCheck<HealthCheckAction1>("Health Test Acion 1", null, new[] { "tag name 1"});
+            services.AddHealthChecksUI().AddInMemoryStorage();
+            services.AddSingleton<HealthCheckAction1>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health", new HealthCheckOptions() 
+                { 
+                    Predicate=_=>true,
+                    ResponseWriter=UIResponseWriter.WriteHealthCheckUIResponse
+                });
+                endpoints.MapHealthChecksUI();
+            });
+        }
+    }
+}
